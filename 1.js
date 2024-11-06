cbb633d70e82305c4c58277e490704c8dd33192e
Katsiaryna Hanchar <kattie@outlook.com>
2024-11-06 15:32:39 +0100

    nothing to commit

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 const factorial = +prompt("Введите число от 1 до 20", "");
 if (factorial < 1 || factorial > 20) {
-  alert("Введите число от 5 до 20");
+  alert("Введите число от 1 до 20");
 } else {
   let result = 1;
   while (factorial > 0) {
